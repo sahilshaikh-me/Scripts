3903c4e5cfd8e65c7a99f9b1525d2b8c60122413
sahillshaikh1 <50543009+sahillshaikh1@users.noreply.github.com>
2021-03-18 21:42:55 +0530

    Add files via upload

---
+++ Main/Tile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public float Speed;
+    public float end;
+    public float start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - Speed * Time.deltaTime);
+        if (transform.position.z <= end)
+        {
+            if (gameObject.tag == "Obstacle" || gameObject.tag == "Destroy")
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, start);
+            }
+          
+        }
+    }
+}
