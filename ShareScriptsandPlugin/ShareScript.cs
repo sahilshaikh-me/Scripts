3a07942f2a498b08ea4cfb6b5d6f8d86685295a5
sahillshaikh1 <50543009+sahillshaikh1@users.noreply.github.com>
2021-03-18 21:38:03 +0530

    Add files via upload

---
+++ ShareScriptsandPlugin/ShareScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VoxelBusters.NativePlugins;
+
+public class ShareScript : MonoBehaviour
+{
+    private string msg;
+    private string Url ="   "+ " https://play.google.com/store/apps/details?id=com.Sahill.Dino3D ";
+    // Start is called before the first frame update
+    void Start()
+    {
+        msg =  "  I cant belive i scored  " + "  " + PlayerPrefs.GetInt("highScore", 0).ToString() + " Click Here To Play This Game ";
+
+    }
+    public void FinishShare(eShareResult result)
+    {
+        Debug.Log("Finish Sharing :"+ result +msg+Url);
+    }
+
+  public void Share()
+    {
+        ShareSheet ss = new ShareSheet();
+        ss.Text = msg;
+        ss.URL = Url;
+
+        NPBinding.UI.SetPopoverPointAtLastTouchPosition();
+        NPBinding.Sharing.ShowView(ss, FinishShare);
+    }
+}
