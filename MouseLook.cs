612f46305c796fbf4e554750e5871b079f6f1884
sahillshaikh1 <50543009+sahillshaikh1@users.noreply.github.com>
2021-08-16 14:45:26 +0530

    FpsWindowsCamera

---
+++ MouseLook.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //FpsCamera
+    public float mouseSensitivity = 100f;
+    public Transform PlayerBody;
+    float xRotation = 0f;
+    void Start()
+    {
+       // Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+
+
+
+}//////
