540a2c3aaec810f5f65bd93d089cf6d58768022c
sahillshaikh1 <50543009+sahillshaikh1@users.noreply.github.com>
2021-08-28 23:59:50 -0800

    Add files via upload

--- AnimationControl.cs
+++ AnimationControl.cs
@@ -4,82 +4,52 @@ using UnityEngine;
 
 public class AnimationControl : MonoBehaviour
 {
-    public float Speed;
-    public bool isExplode;
-  public  List<Vector3> StartPos;
-    [System.Serializable]
-    public class ExplodedViewdataClass
-    {
-        public static ExplodedViewdataClass Instance { get; set; }
-        public GameObject obj;
-        public string name;
-        public enum DirectionTest { Up,Down,Right,Left,Forward,Backward}
-        public int Offset;
-       public DirectionTest direction;
-    }
-    public ExplodedViewdataClass[] ExplodedViewdataClasses;
 
-    //  public int Offset { get;  set; }
-    private void Awake()
-    {
-       
-    }
+    public bool Explode;
+    public MonoBehaviour ExplodeScript;
+    public Animator animController;
+  //  public GameObject ControllerScript;
+
+   // public MonoBehaviour AnimationCon;
     void Start()
     {
-     //   ExplodedViewdataClass.Instance.direction = ExplodedViewdataClass.DirectionTest.Left;
-
-        for (int i = 0; i < ExplodedViewdataClasses.Length; i++)
-        {
-            StartPos.Add(ExplodedViewdataClasses[i].obj.transform.position);
-            ExplodedViewdataClasses[i].name = ExplodedViewdataClasses[i].obj.name;
-        }
-
+        animController = GetComponent<Animator>();
+        
     }
 
-
-    void Update()
+    private void Update()
     {
-        if (isExplode)
+        if (Explode)
         {
-            for (int i = 0; i < ExplodedViewdataClasses.Length; i++)
-            {
-                switch (ExplodedViewdataClasses[i].direction)
-                {
-                    case ExplodedViewdataClass.DirectionTest.Left:
-                        ExplodedViewdataClasses[i].obj.transform.position = Vector3.Lerp(ExplodedViewdataClasses[i].obj.transform.position, new Vector3(-ExplodedViewdataClasses[i].Offset, 0, 0), Time.deltaTime * Speed);
-                        break;
-                    case ExplodedViewdataClass.DirectionTest.Right:
-                        ExplodedViewdataClasses[i].obj.transform.position = Vector3.Lerp(ExplodedViewdataClasses[i].obj.transform.position, new Vector3(ExplodedViewdataClasses[i].Offset, 0, 0), Time.deltaTime * Speed);
-                        break;
-                    case ExplodedViewdataClass.DirectionTest.Up:
-                        ExplodedViewdataClasses[i].obj.transform.position = Vector3.Lerp(ExplodedViewdataClasses[i].obj.transform.position, new Vector3(0, ExplodedViewdataClasses[i].Offset, 0), Time.deltaTime * Speed);
-                        break;
-                    case ExplodedViewdataClass.DirectionTest.Down:
-                        ExplodedViewdataClasses[i].obj.transform.position = Vector3.Lerp(ExplodedViewdataClasses[i].obj.transform.position, new Vector3(0, -ExplodedViewdataClasses[i].Offset, 0), Time.deltaTime * Speed); 
-                        break;
-                    case ExplodedViewdataClass.DirectionTest.Backward:
-                        ExplodedViewdataClasses[i].obj.transform.position = Vector3.Lerp(ExplodedViewdataClasses[i].obj.transform.position, new Vector3(0, 0, -ExplodedViewdataClasses[i].Offset), Time.deltaTime * Speed); 
-                        break;
-                    case ExplodedViewdataClass.DirectionTest.Forward:
-                        ExplodedViewdataClasses[i].obj.transform.position = Vector3.Lerp(ExplodedViewdataClasses[i].obj.transform.position, new Vector3(0, 0, ExplodedViewdataClasses[i].Offset), Time.deltaTime * Speed);
-                        break;
-                }
+           
+          //  AnimationControl.Instance.isExplode = true;
+            animController.SetBool("isPlay", true);
+          //  ControllerScript.SetActive(true);
+            
 
-            }
         }
-        if (!isExplode)
+        else
         {
-            for (int i = 0; i < ExplodedViewdataClasses.Length; i++)
-            {
-                ExplodedViewdataClasses[i].obj.transform.position = Vector3.Lerp(ExplodedViewdataClasses[i].obj.transform.position, StartPos[i],Time.deltaTime*Speed);
-            }
+            animController.SetBool("isPlay", false);
+          //  AnimationControl.Instance.isExplode = false;
+
+          //  ControllerScript.SetActive(false);
         }
+    }
+   
+    public void PlayAnimation()
+    {
+        Explode = !Explode;
        
     }
-
-    public void ExplodeM()
+    public void EnableScript()
     {
-        isExplode = !isExplode;
+        ExplodeScript.enabled = true;
+    }
+    public void DisableScript()
+    {
+        ExplodeScript.enabled = false;
     }
 
-}//////
+
+}/////
